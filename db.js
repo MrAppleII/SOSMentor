19207d6bb3fb0fecbd9d4347cb5d6f9ec2a2d45b
Christian Lapinig <christianlapinig@Christians-MacBook-Pro.local>
2015-10-24 03:29:57 -0700

    Setup MongoDB and added modules

---
+++ db.js
@@ -0,0 +1,13 @@
+var db_uri = '127.0.0.1:27017/praise';
+var collections = ['users'];
+
+if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+  db_uri = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ':' +
+    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + '@' +
+    process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+    process.env.OPENSHIFT_APP_NAME;
+}
+
+var db = require('mongojs')(db_uri, collections);
+
+module.exports = db;
